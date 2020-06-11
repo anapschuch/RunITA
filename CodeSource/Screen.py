389e079dbf43603946ea1e396f085c86e28c2bc8
lucasgameiroborges <37604888+lucasgameiroborges@users.noreply.github.com>
2020-06-11 00:54:58 -0300

    Adapted for new menu

--- CodeSource/Screen.py
+++ CodeSource/Screen.py
@@ -5,8 +5,8 @@ class Screen(object):
     """Classe que seta os parâmetros principais da tela de jogo"""
 
     def __init__(self):
-        self.screen_x = 400
-        self.screen_y = 400
+        self.screen_x = 640
+        self.screen_y = 360
         self.screen = pygame.display.set_mode((self.screen_x, self.screen_y))
         self.start_scroll_x = self.screen_x / 2
         self.start_scroll_y = self.screen_y / 2
